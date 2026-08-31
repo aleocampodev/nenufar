584a7a04d80be26b25de6b51278aed8e558bb41b
Ale <ale@nenufar.co>
2026-08-31 15:08:22 -0500

    fix(loading): make loader take full viewport height to keep footer off-screen during page transitions

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,7 +1,7 @@
 import { NenufarLoader } from '@/components/Loading/NenufarLoader'
 import React from 'react'
 
-export default function Loading() {
+export default function RootLoading() {
   return (
     <div className="min-h-[85vh] flex items-center justify-center bg-[#FAF8F5]">
       <NenufarLoader fullScreen />
